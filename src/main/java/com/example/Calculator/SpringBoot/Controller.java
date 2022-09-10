753f3b2bacd3850c191cc4dcf3898d55be88d5af
JKJohnson0 <104327354+JKJohnson0@users.noreply.github.com>
2022-09-10 18:49:14 -0500

    Initial commit

---
+++ src/main/java/com/example/Calculator/SpringBoot/Controller.java
@@ -0,0 +1,29 @@
+package com.example.Calculator.SpringBoot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+		@GetMapping("/add/{num1}/{num2}")
+		public int addAPI(@PathVariable int num1,@PathVariable int num2) {
+			return num1 + num2;
+		}
+		
+		@GetMapping("/subtract/{num1}/{num2}")
+		public int subtractAPI(@PathVariable int num1,@PathVariable int num2) {
+			return num1 - num2;
+		}
+		
+		@GetMapping("/divide/{num1}/{num2}")
+		public int divideAPI(@PathVariable int num1,@PathVariable int num2) {
+			return num1 / num2;
+		}
+		
+		@GetMapping("/multiple/{num1}/{num2}")
+		public int multipleAPI(@PathVariable int num1,@PathVariable int num2) {
+			return num1 * num2;
+		}
+}
---
+++ src/test/java/com/example/Calculator/SpringBoot/CalculatorSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Calculator.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
